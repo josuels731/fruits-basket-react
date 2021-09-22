9e3647908ec436af5a42a8fd9a2a4b6a84093852
Josue <josue.silva@erzinger.com.br>
2021-09-21 21:15:15 -0300

    Added Global Style

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        user-select: none;
+        margin: 0;
+    }
+
+    body {
+        height: 100vh;
+        width: 100vw;
+        margin: 0;
+
+        box-sizing: border-box;
+        overflow: hidden;
+        
+        font-family: Maven Pro;
+        font-weight: 500;
+            
+        background-color: #06093E;
+        color: #FFFFFF;
+
+        -webkit-font-smoothing: antialiased;
+        -webkit-tap-highlight-color: transparent;
+        -moz-osx-font-smoothing: grayscale;
+    }
+`
+
+export default GlobalStyle
