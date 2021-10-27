635fce6d75a9865920d55e6b4301032935b58341
Josue <josue.silva@erzinger.com.br>
2021-10-27 13:12:53 -0300

    login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,46 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const DesktopLoginArea = styled.div`
+    width: 20vw;
+    height: 50vh;
+
+    background-color: #353762;
+    border-radius: 0.3em;
+`;
+
+// const MobileLoginArea = styled.div`
+//     width: 20vw;
+//     height: 50vh;
+
+//     background-color: #353762;
+//     border-radius: 0.3em;
+// `;
 
 function Login() {
 
-    return <>
-    </>
+    return <Page>
+        <DesktopLoginArea>
+
+            <div>Entrr</div>
+            <div><form action="/" method="get">
+                <input type="text" placeholder='email' />
+                <input type="password" placeholder='senha' />
+                <input type="password" placeholder='senha' />
+                <input type="password" placeholder='senha' />
+                <button type="submit">login</button>
+            </form></div>
+
+        </DesktopLoginArea>
+    </Page>
 }
 
 export default Login;
